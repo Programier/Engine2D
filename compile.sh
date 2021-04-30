30db666e5b30face02e7601eb61a48a0e28b8777
Programier <wladnet28@gmail.com>
2021-04-30 10:41:46 +0300

    Update compilation script

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@ echo "Compiling the game"
 g++ game.cpp Engine2D/*.cpp -o $NAME -lglfw -lpng -lGL -lGLEW -lalut -lopenal -laudio -ldl
 rm -rf lib
 mv Engine2D/lib ./Release
-cp -r $NAME ./Release
+mv $NAME ./Release
 cp -r images ./Release
 cp -r audio ./Release
 echo Done
